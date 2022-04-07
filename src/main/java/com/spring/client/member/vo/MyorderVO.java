1e8801d89f169618f90997b53ab2dc36e73afaec
kimtaeyoon <90013740+mangtaeeee@users.noreply.github.com>
2022-04-07 17:27:13 +0900

    Update MyorderVO.java

--- src/main/java/com/spring/client/member/vo/MyorderVO.java
+++ src/main/java/com/spring/client/member/vo/MyorderVO.java
@@ -8,11 +8,8 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
-@EqualsAndHashCode(callSuper=false)
-@AllArgsConstructor
-@NoArgsConstructor
 public class MyorderVO extends CommonVO{
-	  private long order_num = 0;            // 주문 번호
+	private long order_num = 0;            // 주문 번호
 	   private int order_totalPayment = 0;      // 결제금액
 	   private String order_state = "";      // 주문 상태
 	   private String order_regdate;         // 주문일
