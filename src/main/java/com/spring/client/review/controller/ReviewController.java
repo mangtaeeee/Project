cc775bbfdae460f2eceec4b5424a7dbd3740504a
kimtaeyoon <90013740+mangtaeeee@users.noreply.github.com>
2022-06-20 16:59:24 +0900

    Update ReviewController.java

--- src/main/java/com/spring/client/review/controller/ReviewController.java
+++ src/main/java/com/spring/client/review/controller/ReviewController.java
@@ -1,27 +1,3 @@
-package com.spring.client.review.controller;
-
-import java.util.List;
-
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import com.spring.client.member.vo.MemberVO;
-import com.spring.client.review.service.ReviewService;
-import com.spring.client.review.vo.ReviewVO;
-import com.spring.common.vo.PageDTO;
-
-import lombok.AllArgsConstructor;
-import lombok.Setter;
-import lombok.extern.log4j.Log4j;
 
 @Controller
 @RequestMapping(value="/review/*")
